f5952e9a46e7098ab3b5568d9fc10a9703d6b951
Gary Lin <gml2153@columbia.edu>
2016-01-22 18:57:29 -0500

    makefile myadd ex

---
+++ B-Makefiles/examples/myadd/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "myadd.h"
+
+int main(int argc, char **argv) {
+   int x = myadd(1,2);
+
+   printf("The sum is: %d \n", x);
+
+   return 0;
+
+}
+
---
+++ B-Makefiles/examples/myadd/myadd.h
@@ -0,0 +1,6 @@
+#ifndef __MYADD_H__
+#define __MYADD_H__
+
+int myadd(int a, int b);
+
+#endif
