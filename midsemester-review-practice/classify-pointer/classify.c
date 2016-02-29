4e9d8c1979484e9c97487129efe9fb56fb722773
Gary Lin <gml2153@columbia.edu>
2016-02-28 19:01:58 -0500

    classify

---
+++ midsemester-review-practice/classify-pointer/classify.c
@@ -0,0 +1,54 @@
+//Based on Jae's Fall 2014 Midterm 1
+
+ #include <string.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include "mylist.h"
+
+char *str_dup(const char *str){
+    char *copy = (char *)malloc(strlen(str) + 1);
+    strcpy(copy, str);
+    return copy;
+}
+
+int word_count = 3;
+
+int main(){
+    char *words[word_count];
+    words[0] = "midterm";
+    words[1] = "programming";
+    words[2] = "advanced";
+
+    struct List list;
+    struct List *listPt = &list;
+    initList(listPt);
+
+    int i;
+    for (i = 0 ; i < word_count ; i++){
+        addFront(listPt, str_dup(words[i]));
+    }
+
+    struct Node *node1 = listPt->head;
+    while(node1){
+        printf("%s ", (char*)node1->data);
+        node1 = node1->next;
+    }
+    printf("\n");
+
+    //classify pointers
+    /* (1) */ classify_pointer( words );
+    /* (2) */ classify_pointer( *words );
+    /* (3) */ classify_pointer( listPt );
+    /* (4) */ classify_pointer( listPt->head );
+    /* (5) */ classify_pointer( (&listPt->head) );
+    /* (6) */ classify_pointer( &(listPt->head->next) );
+    /* (7) */ classify_pointer( listPt->head->data );
+    /* (8) */ classify_pointer( &word_count );
+
+    struct Node *node2 = listPt->head;
+    while(node2){
+        free(node2->data);
+        node2 = node2->next;
+    }
+    removeAllNodes(listPt);
+}
---
+++ midsemester-review-practice/classify-pointer/mylist.h
@@ -0,0 +1,137 @@
+#ifndef _MYLIST_H_
+#define _MYLIST_H_
+
+/*
+ * A node in a linked list.
+ */
+struct Node {
+    void *data;
+    struct Node *next;
+};
+
+/*
+ * A linked list.  
+ * 'head' points to the first node in the list.
+ */
+struct List {
+    struct Node *head;
+};
+
+/*
+ * Initialize an empty list.  
+ */
+static inline void initList(struct List *list)
+{
+    list->head = 0;
+}
+
+/*
+ * In all functions below, the 'list' parameter is assumed to point to
+ * a valid List structure.
+ */
+
+/*
+ * Create a node that holds the given data pointer,
+ * and add the node to the front of the list.
+ *
+ * Note that this function does not manage the lifetime of the object
+ * pointed to by 'data'.
+ * 
+ * It returns the newly created node on success and NULL on failure.
+ */
+struct Node *addFront(struct List *list, void *data);
+
+/*
+ * Traverse the list, calling f() with each data item.
+ */
+void traverseList(struct List *list, void (*f)(void *));
+
+/*
+ * Traverse the list, comparing each data item with 'dataSought' using
+ * 'compar' function.  ('compar' returns 0 if the data pointed to by
+ * the two parameters are equal, non-zero value otherwise.)
+ *
+ * Returns the first node containing the matching data, 
+ * NULL if not found.
+ */
+struct Node *findNode(struct List *list, const void *dataSought,
+	int (*compar)(const void *, const void *));
+
+/*
+ * Flip the sign of the double value pointed to by 'data' by
+ * multiplying -1 to it and putting the result back into the memory
+ * location.
+ */
+void flipSignDouble(void *data);
+
+/*
+ * Compare two double values pointed to by the two pointers.
+ * Return 0 if they are the same value, 1 otherwise.
+ */
+int compareDouble(const void *data1, const void *data2);
+
+/*
+ * Returns 1 if the list is empty, 0 otherwise.
+ */
+static inline int isEmptyList(struct List *list)
+{
+    return (list->head == 0);
+}
+
+/*
+ * Remove the first node from the list, deallocate the memory for the
+ * ndoe, and return the 'data' pointer that was stored in the node.
+ * Returns NULL is the list is empty.
+ */
+void *popFront(struct List *list);
+
+/*
+ * Remove all nodes from the list, deallocating the memory for the
+ * nodes.  You can implement this function using popFront().
+ */
+void removeAllNodes(struct List *list);
+
+/*
+ * Create a node that holds the given data pointer,
+ * and add the node right after the node passed in as the 'prevNode'
+ * parameter.  If 'prevNode' is NULL, this function is equivalent to
+ * addFront().
+ *
+ * Note that prevNode, if not NULL, is assumed to be one of the nodes
+ * in the given list.  The behavior of this function is undefined if
+ * prevNode does not belong in the given list.
+ *
+ * Note that this function does not manage the lifetime of the object
+ * pointed to by 'data'.
+ * 
+ * It returns the newly created node on success and NULL on failure.
+ */
+struct Node *addAfter(struct List *list, 
+	struct Node *prevNode, void *data);
+
+/* 
+ * Reverse the list.
+ *
+ * Note that this function reverses the list purely by manipulating
+ * pointers.  It does NOT call malloc directly or indirectly (which
+ * means that it does not call addFront() or addAfter()).
+ *
+ * Implementation hint: keep track of 3 consecutive nodes (previous,
+ * current, next) and move them along in a while loop.  Your function
+ * should start like this:
+
+
+      struct Node *prv = NULL;
+      struct Node *cur = list->head;
+      struct Node *nxt;
+
+      while (cur) {
+          ......
+
+
+ * And at the end, prv will end up pointing to the first element of
+ * the reversed list.  Don't forget to assign it to list->head.
+ */
+void reverseList(struct List *list);
+
+#endif /* #ifndef _MYLIST_H_ */
