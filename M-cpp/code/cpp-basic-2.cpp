0fbc31b10bd434e216426315e4ef1ffa5f76ed38
John Hui <jzh2106@columbia.edu>
2017-11-12 12:03:37 -0500

    added comments to cpp-basic-2.cpp

--- M-cpp/code/cpp-basic-2.cpp
+++ M-cpp/code/cpp-basic-2.cpp
@@ -22,17 +22,22 @@ public:
 
 int main(void)
 {
-    Echoer e0 = Echoer(0);
-    {
+    Echoer e0 = Echoer(0); // created in main() block, not destroyed until end
+
+    {   // new block
         Echoer e1 = Echoer(1);
-    }
+    }   // end block, e1 destructor called
 
     if (1)
         Echoer e2 = Echoer(2);
+        // there is an implicit block at the end of one-liner conditionals
+        // even if there is no end curly brace
 
     int i;
     for (i = 3; i < 6; i++)
         Echoer ei = Echoer(i);
-
+        // same with one-liner looping statements
+        // each ei will be destroyed before the next iteration
+        
     return 0;
-}
+}   // end of main() block, e0 destroyed here
