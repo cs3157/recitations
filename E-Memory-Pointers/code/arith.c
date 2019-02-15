2d96fca66e2bd3e1a9dad7538152938e9e4baa89
Hans Montero <hjm2133@columbia.edu>
2019-02-15 17:10:11 -0500

    Upload pointer arithmetic practice

---
+++ E-Memory-Pointers/code/arith.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(int argc, char **argv) {
+    char *ta = "elsy";
+    printf("%ld\n", (ta + 4) - ta);
+    printf("%ld\n", (int *) (ta + 4) - (int *) (ta));
+    
+    char *tas[] = {"hansy", "aunosy", "nelsy", "chansy"};
+    printf("%ld\n", (tas + 4) - tas);
+    printf("%ld\n", (int *) (tas + 4) - (int *) (tas));
+    
+    char *coolTas[] = {tas[0], tas[3]};
+    printf("%ld\n",  *(coolTas + 1) - *coolTas);
+   
+}
